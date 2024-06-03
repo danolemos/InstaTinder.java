d2152f2125f680f46e3b2e93da51599c4825c97f
danolemos <116593614+danolemos@users.noreply.github.com>
2024-06-03 20:56:56 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa extends Usuario(){
+        String cpf, bio;
+        char sexo;
+        Data nasc;
+        Usuario[] interesses;
+
+        String toString(){
+            return nome + " (" + login + " - " + cpf + ")";
+        };
+
+}
