0f3f70ddb67d46add8ac507ed4969b3ce15a6b52
danolemos <116593614+danolemos@users.noreply.github.com>
2024-06-03 20:57:26 -0300

    Create Postagem.java

---
+++ Postagem.java
@@ -0,0 +1,10 @@
+public class Postagem(){
+    String foto, legenda;
+    Data d;
+
+    void mostrarDados(){
+        System.out.print(foto);
+        System.out.print(legenda);
+        System.out.print(d);
+    }
+}
